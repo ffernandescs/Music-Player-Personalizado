29bbad3796ecd775e39e8296830fa23c952aa221
Felipe Fernandes <f.fernandescs@gmail.com>
2022-03-24 22:08:38 -0300

    Adicionado novas funcionalidades e personalização, a ideia é praticar e compreender o uso de funções, condicionais e manipulação de elementos e objetos ArraY.

--- js/musicList.js
+++ js/musicList.js
@@ -1,40 +1,51 @@
-let listaMusic = [
+let arrayFaixas = [
+    {
+        name: 'Malvadãoaaa',
+        artista: 'Luka Bassaa',
+        img: 'music-1',
+        src: '01'
 
-    {
-        name: "Harley Bird - Home",
-        artist: "Jordan Scho",
-        img: "assets/img/music-1.jpg",
-        src: "assets/music/01.mp3"
     },
     {
-        name: "Ikson Anywhere – Ikson",
-        artist: "Audio Library",
-        img: "assets/img/music-2.jpg",
-        src: "assets/music/02.mp3"
+        name: 'Malvadão',
+        artista: 'Luka Bass',
+        img: 'music-2',
+        src: '02'
+
     },
     {
-        name: "Beauz & Jvna - Crazy",
-        artist: "Beauz & Jvna",
-        img: "assets/img/music-3.jpg",
-        src: "assets/music/03.mp3"
+        name: 'Malvadão',
+        artista: 'Luka Bass',
+        img: 'music-3',
+        src: '03'
+
     },
     {
-        name: "Hardwind - Want Me",
-        artist: "Mike Archangelo",
-        img: "assets/img/music-4.jpg",
-        src: "assets/music/04.mp3"
+        name: 'Malvadão',
+        artista: 'Luka Bass',
+        img: 'music-4',
+        src: '04'
+
     },
     {
-        name: "Jim - Sun Goes Down",
-        artist: "Jim Yosef x Roy",
-        img: "assets/img/music-5.jpg",
-        src: "assets/music/05.mp3"
+        name: 'Malvadão',
+        artista: 'Luka Bass',
+        img: 'music-5',
+        src: '05'
+
     },
     {
-        name: "Lost Sky - Vision NCS",
-        artist: "NCS Release",
-        img: "assets/img/music-6.jpg",
-        src: "assets/music/06.mp3"
+        name: 'Malvadão',
+        artista: 'Luka Bass',
+        img: 'music-5',
+        src: '05'
+
     },
-    
-    ];
+    {
+        name: 'Malvadão',
+        artista: 'Luka Bass',
+        img: 'music-6',
+        src: '06'
+
+    }
+]
--- js/script.js
+++ js/script.js
@@ -5,18 +5,36 @@ let nameArtist =document.querySelector('.nameArtista');
 let imgMusic = document.querySelector('img');
 let prevBtn = document.querySelector('prev');
 let nextBtn = document.querySelector('.next');
-let timeFinal = document.querySelector('.fim');
-let numberFaixa = document.querySelector('.numeroFaixa');
+
+let openCloseListColor = document.querySelector('.buttonColorBackground');
+let clickFora = document.querySelector('.container')
+let openCloseLista = document.querySelector('.listMusic')
+let barraTime = document.querySelector('.barra') 
+let barraProgress = document.querySelector('.timeProgress')
+let fecharMenu = document.querySelector('.fechaMenu');
 
 
 
-music.addEventListener('timeupdate',timeMusic )
 
-let indexMusic = 0;
+let indexMusic = 0
+
+document.addEventListener('mousedown', (event) => {
+    if(clickFora.contains(event.target)) {
+    }
+})
 
 window.addEventListener('load', ()=> {
-    loadMusic(indexMusic);
-});
+    carregarIndex(indexMusic);
+})
+
+function carregarIndex(numbIndex) {
+    nameMusic.innerHTML = arrayFaixas[numbIndex].name;
+    nameArtist.innerHTML = arrayFaixas[numbIndex].artista;
+    imgMusic.src = `assets/img/${arrayFaixas[numbIndex].img}.jpg`;
+    music.src = `assets/music/${arrayFaixas[numbIndex].src}.mp3`;
+
+}
+
 
 function playMusic() {
 
@@ -31,53 +49,79 @@ function playMusic() {
    }
 }
 
-function loadMusic(indexNumb) {
-    nameMusic.innerHTML = listaMusic[indexNumb].name;
-    nameArtist.innerHTML = listaMusic[indexNumb].artist;
-    imgMusic.src = listaMusic[indexNumb].img;
-    music.src = listaMusic[indexNumb].src;
-    
-}
-
-function prevMusic () {
+function prevMusic() {
     indexMusic--;
     if(indexMusic < 0){
         indexMusic = 5
-    } 
-    loadMusic(indexMusic);
-    if(music.paused) {
+    }
+    carregarIndex(indexMusic);
+    if (music.paused) {
         music.play();
         btnPlay.innerHTML = 'pause'
-
-    } else {
-        btnPlay.innerHTML = 'play_arrow'
     }
 }
 
-function nextMusic () {
+function nextMusic() {
     indexMusic++;
     if(indexMusic > 5){
         indexMusic = 0
-    } 
-    loadMusic(indexMusic);
-    if(music.paused) {
+    }
+    carregarIndex(indexMusic);
+    if (music.paused) {
         music.play();
         btnPlay.innerHTML = 'pause'
-    } else {
-        btnPlay.innerHTML = 'play_arrow'
-    }
- }
+    }    
+}
 
- function timeMusic() {
-    let barraTime = document.querySelector('.barra') 
-    barraTime.style.width = Math.floor((music.currentTime / music.duration) * 100) + '%';
-    let timeInitial = document.querySelector('.inicio');
-    timeInitial.innerHTML = campoTimeBarra(Math.floor(music.currentTime));
-    timeFinal.textContent = campoTimeBarra(Math.floor(music.duration));
+music.addEventListener('timeupdate', (e) => {
+    const durationAudio = e.target.currentTime;
+    const secAudio = e.target.duration;
+    let resulTimeAudio = (durationAudio / secAudio) * 100;
+    barraTime.style.width = resulTimeAudio + '%';
 
- }
+    let timeFinal = document.querySelector('.fim');
+    let timeInitial = document.querySelector('.inicio'); 
 
- function campoTimeBarra(seconds) {
+    music.addEventListener('loadeddata', () => {
+        let durationReal = music.duration;
+        let totalMin = Math.floor(durationReal / 60);
+        let totalSeconds = Math.floor(durationReal % 60);
+        if(totalSeconds < 10) {
+            totalSeconds = '0' + totalSeconds;
+        }
+    
+        timeFinal.innerHTML = totalMin + ':' + totalSeconds;
+    
+    });
+    
+        let minCurrent = Math.floor(durationAudio /60);
+        let secCurrent = Math.floor(durationAudio % 60);
+        if(secCurrent < 10) {
+            secCurrent = '0' + secCurrent;
+        }
+    
+        timeInitial.innerHTML = minCurrent + ':' + secCurrent;
+    });
+
+
+
+const ulTag = document.querySelector('.containerList ul');
+
+for (let i = 0; i < arrayFaixas.length; i++) {
+    let liTag = `<li class="numeroFaixa" onclick="playListFaixa()">
+                    <div class="text">
+                        <p class="">${arrayFaixas[i].name}</p>
+                        <p class="">${arrayFaixas[i].artista}</p>
+                    </div>
+                    <audio class="${arrayFaixas[i].src}" src="${arrayFaixas[i].src}" ></audio>
+                    <span class="timeTextTime">1:45</span>
+                </li>`
+ulTag.insertAdjacentHTML('beforeend', liTag);
+
+
+
+
+function campoTimeBarra(seconds) {
     let timeMinuto = Math.floor(seconds / 60);
     let timeSeconds = seconds % 60;
     if(timeSeconds < 10) {
@@ -86,42 +130,56 @@ function nextMusic () {
     return timeMinuto + ':' + timeSeconds;
  }
 
- function clickList () {
-    let btnList = document.querySelector('.listMusic')
-    btnList.style.bottom = '0px';
-    btnList.style.opacity ='1';
- }
- function closeList () {
-    let closeLista = document.querySelector('.listMusic')
-    closeLista.style.bottom = '-55%';
-    closeLista.style.opacity ='0';
- }
-
-const ulTag = document.querySelector('.containerList ul');
-
-for (let i = 0; i < listaMusic.length; i++) {
-    let liTag = `<li class="numeroFaixa" onclick="playListFaixa()">
-                    <div class="text">
-                        <p class="">${listaMusic[i].name}</p>
-                        <p class="">${listaMusic[i].artist}</p>
-                    </div>
-                    <audio class="${listaMusic[i].src}" src="${listaMusic[i].src}" ></audio>
-                    <span id="${listaMusic[i].src}">1:45</span>
-                </li>`
-ulTag.insertAdjacentHTML('beforeend', liTag);
 
 }
 
-function playListFaixa() {
+function fecharMenuDvi() {
+    closeList();
+    closeListColor();
+   fecharMenu.style = 
+   'z-index: -5555;'
+}
 
-    if(music.paused) {
-        music.play();
-        btnPlay.innerHTML = 'pause'
- 
- 
-    }
-     let closeLista = document.querySelector('.listMusic')
-    closeLista.style.bottom = '-55%';
-    closeLista.style.opacity ='0';
+function clickList () {
+    openCloseLista.style.bottom = '0px';
+    openCloseLista.style.opacity ='1';
+        fecharMenu.style = 
+        'z-index: 0;'
+ }
 
+ function closeList () {
+    openCloseLista.style.bottom = '-55%';
+    openCloseLista.style.opacity ='0';
+ }
+
+function closeListColor() {
+        openCloseListColor.style.top = '-55%'     
+    
+}
+
+function mudarColor () {
+    openCloseListColor.style.top = '0'
+    fecharMenu.style = 
+    'z-index: 0;'    
+}
+
+function color1() {
+    clickFora.style = 
+        'background-color: #4C336C;'
+        'box-shadow: 0px 6px 15px rgb(0,0,0,0.342) ;'
+}
+function color2() {
+    clickFora.style = 
+    'background-color: #0e9fff;' +
+    'box-shadow: 0px 6px 15px rgb(0,0,0,0.342);'
+}
+function color3() {
+    clickFora.style = 
+        'background: linear-gradient(#9ce3ff 0%, #fd878d 90%);' +
+        'box-shadow: 0px 6px 15px rgb(0,0,0,0.342);'
+}
+function color4() {
+    clickFora.style = 
+        'background-color: #000;' +
+        'box-shadow: 0px 6px 15px rgb(0,0,0,0.342);'
 }
