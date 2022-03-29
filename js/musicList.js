6ab768fb9fd67557e0ed8c14a2a634d02f4a0fa7
Felipe Fernandes <83374517+ffernandescs@users.noreply.github.com>
2022-03-28 22:44:44 -0300

    Update musicList.js

--- js/musicList.js
+++ js/musicList.js
@@ -1,78 +1,134 @@
 let arrayFaixas = [
     {
-        name: '01',
-        artista: 'Luka ddddsBassaa',
-        img: 'sadsa-1',
-        src: 'music-1'
+        name: '01 - Quando Deus criou voce',
+        artista: 'Banda Ellos',
+        src: '01'
 
     },
     {
-        name: '02',
-        artista: 'Luka Bass',
-        img: 'music-2',
-        src: 'music-2'
+        name: '02 - Só Você',
+        artista: 'Banda Ellos',
+        src: '02'
 
     },
     {
-        name: '03',
-        artista: 'Luka Bass',
-        img: 'music-3',
-        src: 'music-3'
+        name: '03 - Um verso de amor',
+        artista: 'Banda Ellos',
+        src: '03'
 
     },
     {
-        name: '04',
-        artista: 'dsadasdasd Bass',
-        img: 'music-4',
-        src: 'music-4'
+        name: '04 - Escrevi',
+        artista: 'Banda Ellos',
+        src: '04'
 
     },
     {
-        name: '05',
-        artista: 'Luka Bass',
-        img: 'music-5',
-        src: 'music-5'
+        name: '05 - Amor Verdadeiro',
+        artista: 'Banda Ellos',
+        src: '05'
 
     },
     {
-        name: '06',
-        artista: 'Luka Bass',
-        img: 'music-6',
-        src: 'music-6'
+        name: '06 - Dia do Casamento',
+        artista: 'Banda Ellos',
+        src: '06'
 
     },
     {
-        name: '07',
-        artista: 'dsadasdadddsd Bass',
-        img: 'music-7',
-        src: 'music-7'
+        name: '07 - Pensando em Você',
+        artista: 'Banda Ellos',
+        src: '07'
 
     },
     {
-        name: '08',
-        artista: 'Luka Bass',
-        img: 'music-8',
-        src: 'music-8'
+        name: '08 - Você me conquistou',
+        artista: 'Banda Ellos',
+        src: '08'
 
     },
     {
-        name: '09',
-        artista: 'Luka Bass',
-        img: 'music-9',
-        src: 'music-9'
+        name: '09 - Trabalhai e Orai',
+        artista: 'Banda Ellos',
+        src: '09'
 
     },
     {
-        name: '10',
-        artista: 'Luka Bass',
-        img: 'music-10',
-        src: 'music-10'
+        name: '10 - Alma abatida',
+        artista: 'Banda Ellos',
+        src: '10'
 
     },
     {
-        name: '11',
-        artista: 'Luka Bass',
-        img: 'music-11',
-        src: 'music-11'
+        name: '11 - Ousado Amor',
+        artista: 'Banda Ellos',
+        src: '11'
+
+    },
+    {
+        name: '12 - Não há Limites',
+        artista: 'Banda Ellos',
+        src: '12'
+
+    },
+    {
+        name: '13 - Os anjos te Louvam',
+        artista: 'Eli Soares',
+        src: '13'
+
+    },
+    {
+        name: '14 - Videira',
+        artista: 'Banda Ellos',
+        src: '14'
+
+    },
+    {
+        name: '15 - Lá vem Faraó',
+        artista: 'Banda Ellos',
+        src: '15'
+
+    },
+    {
+        name: '16 - Aos pés da Cruz',
+        artista: 'Eli Soares',
+        src: '16'
+
+    },
+    {
+        name: '17 - Amar Você',
+        artista: 'Banda Ellos',
+        src: '17'
+
+    },
+    {
+        name: '18 - Lágrimas no Olhar',
+        artista: 'Banda Ellos',
+        src: '18'
+
+    },
+    {
+        name: '19 - Por Amor-O Dom De Amar-Tudo Vem De Você',
+        artista: 'Catedral',
+        src: '19'
+
+    },
+    {
+        name: '20 - O tempo não pode apagar',
+        artista: 'Cassiane',
+        src: '20'
+
+    },
+    {
+        name: 'Te amaria outra vez',
+        artista: 'Banda Ellos',
+        src: '21'
+
+    },
+    {
+        name: 'Sempre te amarei',
+        artista: 'Banda Ellos',
+        src: '22'
+
     },
 ]
